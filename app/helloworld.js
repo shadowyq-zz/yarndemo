3f20eb977397496ff46ccd359253c211a6eac463
shadow <yangqiu0928@126.com>
2017-05-12 15:20:48 +0800

    Demo

--- app/helloworld.js
+++ app/helloworld.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import styles from './style.css';
 class Hello extends React.Component{
-	reader(){
+	constructor(props){
+		super(props);
+		this.state = {
+			name: ''
+		}
+	}
+	render(){
 		return(
-			<div>hello world</div>
-			)
+			<div className={styles.root}>
+			<h2>用户登录</h2>
+			<form ref='addForm' action="#">
+				<p>
+					<label htmlFor="name">姓名:</label><input type="test" id="name"  ref="name" placeholder="请输入用户名"/>
+				</p>
+				<p>
+	               <label htmlFor="pass"> 密码:</label><input type="password" id="pass" ref="pass" placeholder="请输入用户名"/>
+	            </p>
+
+	            <input type="submit" value="登录" onClick={this._login.bind(this)} className={styles.button}/>
+
+	            <input type="reset" value="清空" className={styles.button}/>
+	            <div>
+	            	{this.state.name?"欢迎"+this.state.name+"登录！！！":null}
+	            </div>
+	        </form>
+	        </div>
+		)
+		
+	}
+	_login(){
+		let name = this.refs.name.value;
+		let pass = this.refs.pass.value;
+		if(name.length == 0 || pass.length == 0){
+			alert("用户名或密码不能为空")
+		}else{
+			this.setState({name:name})
+		}
 	}
 }
-module.exports = Hello
+export default Hello
 
--- app/main.js
+++ app/main.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './helloworld.js';
+import Test from './test.js';
+var names = ['Alice','Emily','Kate'];
 ReactDOM.render(
-	<h1>Hello React</h1>,
+	<div>
+	{
+		names.map(function(name,i){
+			return <div key={i}>aaa, {name}!</div>
+		})
+	}
+	</div>,
 	document.getElementById('container')
 );
 
+
+var HelloMessage = React.createClass({
+	render: function(){
+		return <h1>Hello {this.props.name}</h1>;
+	}
+});
